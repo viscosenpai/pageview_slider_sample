3d1395bab1c470526cad5187d4645b431258b019
viscosenpai <kama.hiroki@gmail.com>
2022-01-07 01:39:11 +0900

    [add] controll button and infinite scroll

--- lib/app.dart
+++ lib/app.dart
@@ -4,19 +4,58 @@ import 'pages/page2.dart';
 import 'pages/page3.dart';
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  App({Key? key}) : super(key: key);
+
+  final List<Widget> pages = [
+    const Page1(),
+    const Page2(),
+    const Page3(),
+  ];
+
+  final controller = PageController(initialPage: 999);
+  final _kDuration = const Duration(milliseconds: 300);
+  final _kCurve = Curves.ease;
 
   @override
   Widget build(BuildContext context) {
-    final controller = PageController();
     return Scaffold(
       body: SafeArea(
-        child: PageView(
-          controller: controller,
-          children: const <Widget>[
-            Page1(),
-            Page2(),
-            Page3(),
+        child: Stack(
+          children: <Widget>[
+            PageView.builder(
+              controller: controller,
+              itemBuilder: (
+                BuildContext context,
+                int index,
+              ) {
+                return pages[index % pages.length];
+              },
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      controller.previousPage(
+                        duration: _kDuration,
+                        curve: _kCurve,
+                      );
+                    },
+                    child: const Text('prev'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      controller.nextPage(
+                        duration: _kDuration,
+                        curve: _kCurve,
+                      );
+                    },
+                    child: const Text('next'),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
